e735985e7d9e5a5b9e11a0bdde8a790008d3362b
Upal <upal.chowdhury@outlook.com>
2024-05-12 18:16:19 -0400

    Update manyfiles.sh

--- src/manyfiles.sh
+++ src/manyfiles.sh
@@ -1,7 +1,15 @@
-aws s3api list-objects-v2 \
-    --bucket your-bucket-name \
-    --query "Contents | sort_by(@, &LastModified)[::-1][:100].{Key: Key}" \
-    --output text > files_to_download.txt
-while IFS= read -r file_key; do
-    aws s3 cp "s3://your-bucket-name/$file_key" "/local/path/to/download/$file_key"
-done < files_to_download.txt
+#!/bin/bash
+
+# Define your bucket name
+BUCKET_NAME="your-bucket-name"
+
+# Get the list of files sorted by modification date
+aws s3api list-objects-v2 --bucket $BUCKET_NAME --query 'Contents | sort_by(@, &LastModified)[-100:].{Key: Key}' --output text > filelist.txt
+
+# Download the last 100 modified files
+tail -n 100 filelist.txt | while read -r line; do
+  aws s3 cp s3://$BUCKET_NAME/"$line" ./downloaded/
+done
+
+# Cleanup
+rm filelist.txt
